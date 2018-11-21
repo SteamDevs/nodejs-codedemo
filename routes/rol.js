8c317e87e65f0ec565e0edabfb3cf7ecb80717ff
Pablo Jacobo <pablojacobo1998@gmail.com>
2018-11-21 11:46:45 -0600

    Refactor Async/Await

--- routes/rol.js
+++ routes/rol.js
@@ -3,26 +3,29 @@ const app = express()
 
 let model_rol = require('../model/rol')
 
-app.get('/',  (req, res)=>{
-
-    model_rol.getDataRol( (err, rolDB)=>{
-        if(err){
-            res.send({ message: 'Hubo un error', errors: err })
-        }
-
-        res.send({ message: 'Consutla exitosa', resultados: rolDB }).status(200)
-    })
+app.get('/',  async (req, res)=>{
+    try {
+        let rl = await model_rol.getDataRol( (err, rolDB)=>{
+            if(err) res.send({message: 'eror de base de datos'})
+            res.send({message: ' consulta exitosa', results: rolDB })
+        })
+    } catch (err) {
+        console.log(err);
+    }
 })
 
-app.post('/', (req, res)=>{
-
-    model_rol.insertRol( req.body, (newRol)=>{
-        res.status(200).json({
-            ok: true,
-            message: 'Peticion Exitosa',
-            results: newRol
+app.post('/', async (req, res)=>{
+    try {
+        let rl = await model_rol.insertRol(req.body, (newRol)=>{
+            res.status(200).json({
+                ok: true,
+                message: 'Consulta exitosa',
+                results: newRol
+            })
         })
-    })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 module.exports = app
