912feeff5f146b4f368797345ea679424ef4ee03
Pablo Jacobo <pablojacobo1998@gmail.com>
2018-12-03 15:01:01 -0600

    Prepare-Models

--- model/async.js
+++ model/async.js
@@ -14,17 +14,24 @@ let rol = {
 
     insertData: async(data)=>{
         if(db){
-            let values = [ data.tipo, data.id_users ]
-            let sql = `INSERT INTO rol(tipo, id_users) VALUES(?, ?)`
+            let values = [ data.tipo, data.id_users]
+            let sql = `INSERT INTO rol(tipo, id_users) VALUES(?, ?)`          
             const INSERT_ROL = await database.query(sql, values, (err, result)=>{
                 if(err) throw err;
                     let data = INSERT_ROL.val()
                     return data 
-            })    
+            })
+
         }
+    },
+
+    deleteData: async(id)=>{
+        console.log('llegaste a eliminar algun datos')
+    },
+
+    updateData: async(data)=>{
+        console.log('Ibas a actualizar un dato')
     }
-
-
 }
 
 module.exports = rol;
