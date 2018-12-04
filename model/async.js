cbd7f79dbd6103584d4b29e6a4fcafa72a3b4769
Pablo Jacobo <pablojacobo1998@gmail.com>
2018-12-04 14:38:00 -0600

    Async Model/Route Demo

--- model/async.js
+++ model/async.js
@@ -3,7 +3,7 @@ const db = require('../www/config')
 let rol = {
     getAll: async (data) => {
         if(db){
-            let sql = `SELECT * FROM rol`
+            let sql = `SELECT * FROM async`
             const GET_ROL = await db.query(sql, data, (err, results)=>{
                 if(err) throw err;
                 let data = GET_ROL.val()
@@ -12,23 +12,36 @@ let rol = {
         }
     },
 
-    insertData: async(data, newRol )=>{
+    insertData: async(data, newAsy)=>{
         if(db){
-            let values = [ data.tipo, data.id_users]
-            let sql = `INSERT INTO rol(tipo, id_users) VALUES(?, ?)`          
-            let rl = await db.query(sql, values, (err, result)=>{
+            let values = [ data.tema, data.descripcion]
+            let sql = `INSERT INTO async(tema, descripcion) VALUES(?, ?)`          
+            let asy = await db.query(sql, values, (err, result)=>{
                 if(err) throw err;
-                return newRol(result) 
+                return newAsy(result) 
             })
         }
     },
 
-    deleteData: async(id)=>{
-        console.log('llegaste a eliminar algun datos')
+    deleteData: async(id, deleteAsy)=>{
+        if(db){
+            let sql = `DELETE FROM async WHERE id_async = ?`
+            let asy = await db.query(sql, id, (err, results)=>{
+                if(err) throw err;
+                deleteAsy(results)
+            })
+        }
     },
 
-    updateData: async(data)=>{
-        console.log('Ibas a actualizar un dato')
+    updateData: async(data, updateAsy)=>{
+        if(db){
+            let values = [ data.tema, data.descripcion, data.id ]
+            let sql = `UPDATE async SET tema = ?, descripcion = ? WHERE id_async = ?`
+            let asy = await db.query(sql, values, (err, results)=>{
+                if(err) throw err;
+                updateAsy(results)
+            })
+        }
     }
 }
 
--- routes/async.js
+++ routes/async.js
@@ -4,25 +4,69 @@ const asyncModel = require('../model/async')
 
 app.get('/', async(req, res)=>{
     try {
-        let rl = await asyncModel.getAll( (err, asyncDB)=>{
+        
+        let asy = await asyncModel.getAll( (err, asyncDB)=>{
             if(err) res.send({message: 'eror de base de datos'})
             res.send({message: ' consulta exitosa', results: asyncDB })
         })
+
     } catch (err) {
-        console.log(err)
+        return console.log(err)
     }
 })
 
 app.post('/', async(req, res)=>{
     try {
-        let rl = await asyncModel.insertData(req.body, (newAsync)=>{
+        
+        let asy = await asyncModel.insertData(req.body, (newAsync)=>{
             res.status(200).json({
                 message: 'Consulta exitosa',
                 results: newAsync 
             })    
-        })            
+        })
+
     } catch (err) {
         return console.log(err);
+    }
+})
+
+app.delete('/:id', async(req, res)=>{
+    try {
+        
+        let id = req.params.id
+
+        let asy = await asyncModel.deleteData(id, (deleteData)=>{
+            if(!deleteData){
+                res.send({ message: 'No pasaste nada por parametro'})
+            }
+            
+            res.status(200).json({
+                ok: true,
+                message: 'El resistro se elimino exitosamente',
+                results: deleteData
+            })
+        })
+
+    } catch (err) {
+        return console.log(err)    
+    }
+})
+
+app.put('/:id', async(req, res)=>{
+    try {
+        
+        req.body.id = req.params.id
+
+        let asy = await asyncModel.updateData(req.body, (updateData)=>{
+            res.status(200).json({
+                ok: true,
+                message: 'Registro Actualizado',
+                results: updateData
+            })
+        })
+        
+    } catch (err) {
+        console.log(err)
     }
 })
 
